473b5711e52da866c5438aff63f60f8aceca690d
Aleksander Maj <Aleksander.Maj@cybercom.com>
2017-07-06 13:49:13 +0200

    Initial Commit

---
+++ ViperSideDrawer/ViperSideDrawer.h
@@ -0,0 +1,19 @@
+//
+//  ViperSideDrawer.h
+//  ViperSideDrawer
+//
+//  Created by Aleksander Maj on 06/07/2017.
+//  Copyright © 2017 Aleksander Maj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViperSideDrawer.
+FOUNDATION_EXPORT double ViperSideDrawerVersionNumber;
+
+//! Project version string for ViperSideDrawer.
+FOUNDATION_EXPORT const unsigned char ViperSideDrawerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViperSideDrawer/PublicHeader.h>
+
+
